b2f8740c1c14af4b237a8b764c5fe56f5110b4d3
JadianRadiator <jadianradiator@gmail.com>
2025-04-13 07:46:45 -0500

    update MainActivity.kt

--- app/src/main/java/com/example/webviewapp/MainActivity.kt
+++ app/src/main/java/com/example/webviewapp/MainActivity.kt
@@ -1,53 +1,21 @@
 package com.example.webviewapp
 
-import android.content.Intent
-import android.net.Uri
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.webkit.*
-import androidx.appcompat.app.AlertDialog
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private val BASE_URL = "https://www.slavehack2.com/"
-
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        val webView = WebView(this)
-        setContentView(webView)
-
+        val webView: WebView = findViewById(R.id.webView)
         webView.settings.javaScriptEnabled = true
-        webView.settings.domStorageEnabled = true
-        webView.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
-                val url = request.url.toString()
-                return if (url.startsWith(BASE_URL)) {
-                    false
-                } else {
-                    showExternalLinkDialog(url)
-                    true
-                }
-            }
-        }
-
-        val data: Uri? = intent?.data
-        if (data != null) {
-            webView.loadUrl(data.toString())
-        } else {
-            webView.loadUrl(BASE_URL)
-        }
-    }
-
-    private fun showExternalLinkDialog(url: String) {
-        AlertDialog.Builder(this)
-            .setTitle("Open External Link")
-            .setMessage("This link is outside the app. Do you want to continue?")
-            .setPositiveButton("Yes") { _, _ ->
-                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-                startActivity(browserIntent)
-            }
-            .setNegativeButton("No", null)
-            .show()
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://slavehack2.com")
     }
 }
